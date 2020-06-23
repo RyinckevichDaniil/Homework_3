45e2034c83df83939634c7cde178240b1c6407d4
RyinckevichDaniil <abdul.alahakbar@gmail.com>
2020-06-23 21:51:20 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-// var age = Number( prompt("Сколько вам лет", "19 лет"))
-
-//     var ageNumber = age > 10 ? age % 10 : age;
-
-
-
-//         if (ageNumber === 1) {
-//             console.log(age + ' год')
-//         } else if (age > 1 && age < 5 ) {
-//             console.log(age +  ' года');
-//         } else if (ageNumber > 1 && age < 5 ) {
-//             console.log(age +  ' года');
-//         } else {
-//             console.log(age + ' лет');
-//         }
 var age = Number( prompt("Сколько вам лет"))
 
 var ageNumber = age > 20 ? age % 10 : age;
